56fb885048e899f23a958958f709c76a7b46d16d
ADEDEJI OGUNDIPE <ogundipeadedeji@gmail.com>
2022-02-18 12:52:07 +0100

    Modified Testscript and ground color

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("Hello LASRIC!");
     }
 
     // Update is called once per frame
